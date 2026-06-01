122c44ffb3d66e870b44906570e56d1985a6c19b
MADHUMALKA W.K. <madhumalkawk.22@uom.lk>
2026-06-01 13:51:07 +0530

    Move backend to root AgriLink.API, add .NET gitignore rules

--- backend/AgriLink.API/Services/ICloudinaryService.cs
+++
@@ -1,7 +0,0 @@
-namespace AgriLink.API.Services;
-
-public interface ICloudinaryService
-{
-    Task<string> UploadImageAsync(IFormFile file, string folderName);
-    Task<bool> DeleteImageAsync(string publicId);
-}
